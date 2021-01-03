4bb045531b9892758a2292cfdbfa757394fbaa0e
KiharaSimon <symokihara@gmail.com>
2021-01-03 23:00:27 +0300

    Condition

---
+++ conditons.rb
@@ -0,0 +1,16 @@
+ismale = false
+isshort = false
+
+if ismale and isshort
+  puts "This is a short male"
+
+elsif ismale and !isshort
+  puts "This is a tall male"
+
+elsif !ismale and isshort
+  puts "This is a short female"
+
+else !ismale and !isshort
+  puts "This is a tall female"
+
+end
