c214688af345e483eae750f8c68c2f5edc8d04ba
KiharaSimon <symokihara@gmail.com>
2021-01-05 13:00:50 +0300

    Game completion

--- guessing.rb
+++ guessing.rb
@@ -2,18 +2,21 @@
 secret_word = "giraffe"
 guess = ""
 guess_count = 0
-guess_limit = 5
+guess_limit = 3
+out_of_guess = false
 
-while guess != secret_word
+while guess != secret_word and !out_of_guess
   if (guess_count < guess_limit)
     puts "Enter the guess as the name of an animal"
     guess = gets.chomp()
     guess_count += 1
   else
-    puts "You are out of guesses"
+    out_of_guess = true
   end
-
+end
+if out_of_guess == true
+  puts "You Loose"
+else
+puts "You Won!"
 
 end
-
-puts "You Won!"
