a20b6e1206e4a9cf0416f829e2078fa2cb59dd48
KiharaSimon <symokihara@gmail.com>
2021-01-05 12:51:02 +0300

    Adding guessing limits and counts

--- guessing.rb
+++ guessing.rb
@@ -1,10 +1,18 @@
 
 secret_word = "giraffe"
 guess = ""
+guess_count = 0
+guess_limit = 5
 
 while guess != secret_word
-  puts "Enter the guess as the name of an animal"
-  guess = gets.chomp()
+  if (guess_count < guess_limit)
+    puts "Enter the guess as the name of an animal"
+    guess = gets.chomp()
+    guess_count += 1
+  else
+    puts "You are out of guesses"
+  end
+
 
 end
 
