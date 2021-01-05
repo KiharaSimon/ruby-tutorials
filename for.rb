ebb2c0ae6e34264ce8abfcac36490f6f52457cbd
KiharaSimon <symokihara@gmail.com>
2021-01-05 14:20:01 +0300

    Loops

---
+++ for.rb
@@ -0,0 +1,11 @@
+
+clothes_brand = ["Gucci", "Versace", "Mane", "Polo"]
+planets = ["Simon", "Jupiter", "Saturn", "Earth"]
+
+for cloth in clothes_brand
+  puts cloth
+end
+
+planets.each do |planet|
+  puts planet
+end
