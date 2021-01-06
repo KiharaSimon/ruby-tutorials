62a6330915ca8dffaaac848ee2be139b095efe89
KiharaSimon <symokihara@gmail.com>
2021-01-06 10:28:01 +0300

    File processing

--- file.rb
+++ file.rb
@@ -1,4 +1,17 @@
-File.open("names.txt", "r") do |names|
+File.open("names.txt", "a") do |names|
 
-  puts names.read()
+  # puts names.read()
+
+  names.write("James, writer")
+
+
+
 end
+
+=begin
+The methods associated with reading and writing a
+File.read("path/to/file")
+File.write
+File.readLine()
+File.readChar()
+=end
