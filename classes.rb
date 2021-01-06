d9cc5f24899c68cd7ee6ea9765117408a9ce95ca
KiharaSimon <symokihara@gmail.com>
2021-01-06 11:08:51 +0300

    Classes and deriving objects from classes

---
+++ classes.rb
@@ -0,0 +1,12 @@
+ class Citizen
+   attr_accessor :name, :race, :occupation
+
+ end
+
+ citizen1 = Citizen.new()
+citizen1.name = "James Connen"
+citizen1.race = "Latino"
+citizen1.occupation = "Farmer"
+
+
+puts citizen1.name
